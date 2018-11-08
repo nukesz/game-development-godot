bf3b5ca6e12bfe1493dc869cdbc6fb94504eb3eb
Norbert Benczur <norbert.benczur@gmail.com>
2018-11-08 21:05:45 +0100

    Added the example project for the First Game on Godot Doc

---
+++ OfficialFirstGame/Player.gd
@@ -0,0 +1,37 @@
+extends Area2D
+
+export (int) var speed  # How fast the player will move (pixels/sec).
+var screensize  # Size of the game window.
+
+
+func _ready():
+    screensize = get_viewport_rect().size
+	
+func _process(delta):
+    var velocity = Vector2() # The player's movement vector.
+    if Input.is_action_pressed("ui_right"):
+        velocity.x += 1
+    if Input.is_action_pressed("ui_left"):
+        velocity.x -= 1
+    if Input.is_action_pressed("ui_down"):
+        velocity.y += 1
+    if Input.is_action_pressed("ui_up"):
+        velocity.y -= 1
+    if velocity.length() > 0:
+        velocity = velocity.normalized() * speed
+        $AnimatedSprite.play()
+    else:
+        $AnimatedSprite.stop()
+		
+    position += velocity * delta
+    position.x = clamp(position.x, 0, screensize.x)
+    position.y = clamp(position.y, 0, screensize.y)
+	
+    if velocity.x != 0:
+    	$AnimatedSprite.animation = "right"
+    	$AnimatedSprite.flip_v = false
+    	# See the note below about boolean assignment
+    	$AnimatedSprite.flip_h = velocity.x < 0
+    elif velocity.y != 0:
+    	$AnimatedSprite.animation = "up"
+    	$AnimatedSprite.flip_v = velocity.y > 0
