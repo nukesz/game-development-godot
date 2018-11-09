9b590715f47e1f930c277cf194d05132b47d6c67
Norbert Benczur <norbert.benczur@gmail.com>
2018-11-09 20:26:40 +0100

    Added Main scene

---
+++ OfficialFirstGame/Main.gd
@@ -0,0 +1,46 @@
+extends Node
+
+export (PackedScene) var Mob
+var score
+
+func _ready():
+    randomize()
+    new_game()
+
+func game_over():
+    $ScoreTimer.stop()
+    $MobTimer.stop()
+
+func new_game():
+    score = 0
+    $Player.start($StartPosition.position)
+    $StartTimer.start()
+
+func _on_StartTimer_timeout():
+	print("_on_StartTimer_timeout")
+	$MobTimer.start()
+	$ScoreTimer.start()
+
+func _on_ScoreTimer_timeout():
+	print("_on_ScoreTimer_timeout")
+	score += 1
+	
+func _on_MobTimer_timeout():
+    print("Spawning mob")
+	# Choose a random location on Path2D.
+    $MobPath/MobSpawnLocation.set_offset(randi())
+    # Create a Mob instance and add it to the scene.
+    var mob = Mob.instance()
+    add_child(mob)
+    # Set the mob's direction perpendicular to the path direction.
+    var direction = $MobPath/MobSpawnLocation.rotation + PI / 2
+    # Set the mob's position to a random location.
+    mob.position = $MobPath/MobSpawnLocation.position
+    # Add some randomness to the direction.
+    direction += rand_range(-PI / 4, PI / 4)
+    mob.rotation = direction
+    # Choose the velocity.
+    mob.set_linear_velocity(Vector2(rand_range(mob.min_speed, mob.max_speed), 0).rotated(direction))
+
+
+
