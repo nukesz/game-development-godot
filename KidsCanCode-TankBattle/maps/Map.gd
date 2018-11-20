223f624be716c6352151607359e1e145e311484d
Norbert Benczur <norbert.benczur@gmail.com>
2018-11-20 21:58:33 +0100

    Added Bullets to the game

--- KidsCanCode-TankBattle/maps/Map.gd
+++ KidsCanCode-TankBattle/maps/Map.gd
@@ -12,3 +12,8 @@ func set_camera_limits():
     $Player/Camera2D.limit_bottom = map_limits.end.y * map_cellsize.y
     print(map_limits)
     print(map_cellsize)
+
+func _on_Tank_shoot(bullet, _position, _direction):
+    var b = bullet.instance()
+    add_child(b)
+    b.start(_position, _direction)
