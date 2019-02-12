ba2c9b87d4e5420a363c34caffdc03619d87141c
Norbert Benczur <norbert.benczur@gmail.com>
2019-02-12 21:17:08 +0100

    Added explosion and changed the default cursor

--- KidsCanCode-TankBattle/bullets/Bullet.gd
+++ KidsCanCode-TankBattle/bullets/Bullet.gd
@@ -16,7 +16,11 @@ func _process(delta):
     position += velocity * delta    
 
 func explode():
-    queue_free()
+    velocity = Vector2()
+    $Sprite.hide()
+    $Explosion.show()
+    $Explosion.play('smoke')
+    
 
 func _on_Bullet_body_entered(body):
     explode()
@@ -27,3 +31,7 @@ func _on_Bullet_body_entered(body):
 func _on_Lifetime_timeout():
     print("Timeout")
     explode()
+
+
+func _on_Explosion_animation_finished():
+    queue_free()
