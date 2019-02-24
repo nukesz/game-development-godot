8f021f8afecb72a577b46ce915c77e8c5c63114d
Norbert Benczur <norbert.benczur@gmail.com>
2019-02-24 22:31:52 +0100

    Added pickups to the map

--- KidsCanCode-TankBattle/Pickup.gd
+++ KidsCanCode-TankBattle/Pickup.gd
@@ -3,5 +3,15 @@ extends Area2D
 enum Items { health, ammo }
 
 export (Items) var type = Items.health
-export (Vector2) var ammo = Vector2(10, 25) # random value between
+export (Vector2) var amount = Vector2(10, 25) # random value between
 
+
+
+func _on_Pickup_body_entered(body):
+    match type:
+        Items.health:
+            if body.has_method('heal'):
+                body.heal(int(rand_range(amount.x, amount.y)))
+        Items.ammo:
+            pass
+    queue_free()
