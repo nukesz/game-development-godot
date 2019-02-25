571b20f62816de2dc040d6bdd96b28a73c61a1d4
Norbert Benczur <norbert.benczur@gmail.com>
2019-02-25 20:32:46 +0100

    Added ammo texture

--- KidsCanCode-TankBattle/Pickup.gd
+++ KidsCanCode-TankBattle/Pickup.gd
@@ -2,10 +2,14 @@ extends Area2D
 
 enum Items { health, ammo }
 
+var icon_textures = [preload("res://assets/effects/wrench_white.png"),
+                     preload("res://assets/effects/ammo_machinegun.png")]
+
 export (Items) var type = Items.health
 export (Vector2) var amount = Vector2(10, 25) # random value between
 
-
+func _ready():
+    $Icon.texture = icon_textures[type]
 
 func _on_Pickup_body_entered(body):
     match type:
@@ -13,5 +17,5 @@ func _on_Pickup_body_entered(body):
             if body.has_method('heal'):
                 body.heal(int(rand_range(amount.x, amount.y)))
         Items.ammo:
-            pass
+            body.ammo += int(rand_range(amount.x, amount.y))
     queue_free()
