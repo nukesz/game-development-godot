7683388337868c485510150e35f536c22be8ac3b
Norbert Benczúr <norbert.benczur@gmail.com>
2019-03-28 19:02:18 +0100

    Created enemy scene

---
+++ EscapeTheMaze/character/Enemy.gd
@@ -0,0 +1,13 @@
+extends "res://character/Character.gd"
+
+func _ready():
+    can_move = false
+    facing = moves.keys()[randi() * 4]
+    yield(get_tree().create_timer(0.5), 'timeout')
+    can_move = true    
+    
+func _process(delta: float):
+    if can_move:
+        if not move(facing) or randi() % 10 > 5:
+            facing = moves.keys()[randi() * 4]    
+    
